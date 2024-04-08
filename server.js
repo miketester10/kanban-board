64e627040a0e14241b49168564431ee2a72145fc
miketester10 <cadenza91@gmail.com>
2024-04-08 04:41:03 +0200

    Implementato JWT Token con algoritmo di crittografia asimmetrica RSA

--- server.js
+++ server.js
@@ -1,55 +1,46 @@
 'use strict';
 
+const fs = require('fs');
 const express = require('express');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy; // strategia username(email)+password
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const task_dao = require('./db/kanban_dao');
 const utenti_dao = require('./db/utenti_dao');
 require('dotenv').config();
 
-/*** Set up Passport ***/
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-        console.log( `*** email: ${username}, password: ${password} ***`);
-        utenti_dao.getUserFromDBbyEmail(username, password).then((user) => {
-            
-            if (!user) {
-                console.log('Email o password errata!');
-                return done(null, false); // mettendo false restituisce uno status code 401 Unauthorized
-            } else {
-                console.log('Login effettuato con successo!');
-                return done(null, user); // restituisce l'oggetto user alla route 
-            }
-        
-        }).catch((err) => {
-            console.error('Errore catturato: ', err);
-            return done(null, false); // mettendo false restituisce uno status code 401 Unauthorized
-        });
-}));
 
 /*** Inizializzo Express ***/
 const app = express();
 const PORT = 8080;
 
+/*** JWT options ***/
+const option = { expiresIn: '1h', algorithm: 'RS256' };
+
 /*** Set up Middleware ***/
 app.use(express.json());
 app.use(express.static('public')); // configuro Express per servire i file statici dalla cartella 'public'.
-
-/*** Abilito Passport per usare le sessioni ****/
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cookieParser());
 
 /*** Definisco tutte le Route ***/
 // Route per recuperare tutte le tasks dell'utente dal DB [METHOD = GET]
 app.get('/api/v1/tasks', async (req, res) => { 
     try {
-        const user = req.user; // req.user contiene l'oggetto user creato con deserializeUser se il login è andato a buon fine
-        const id = user.id; 
+        const token = req.cookies.token;
+        const pub_key = fs.readFileSync('./key/rsa.public', 'utf8');
+        if (!token) return res.status(401).json({ error: 'Utente non autenticato!' });
+        const payload = jwt.verify(token, pub_key, option);
+        console.log(payload);
+        const id = payload.id;
+        const user = payload; 
         const tasks = await task_dao.getTasksFromDB(id);
         console.log(tasks);
         res.json({ tasks, user });    
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        if (error.message.includes('jwt expired') || error.message.includes('invalid signature')) {
+            res.status(401).json({ error: 'Utente non autenticato! Token scaduto o non valido.' });
+        } else {
+            res.status(500).json({ error: error.message });
+        };
     };
 });
 
@@ -93,10 +84,25 @@ app.post('/iscrizione', async (req, res) => {
 });
 
 // Route per il login/autenticazione [METHOD = POST]
-app.post('/login', (req, res) => {
-    // console.log(req);
-    // console.log(req.user.username);
-    res.json( req.user ); // dentro req.user ci sarà { id: row.id, username: row.email, name: row.nome } ovvero l'oggetto ricevuto dal DB dopo aver fatto richiesta con LocalStrategy
+app.post('/login', async (req, res) => {
+    try {
+        const email = req.body.username;
+        const password = req.body.password;
+        const user = await utenti_dao.getUserFromDBbyEmail(email, password);
+        console.log(user);
+        if (!user) return res.status(401).json({ error: 'Email o password errata!' });
+        const payload = { id: user.id, nome: user.nome };
+        const cookieOption = { 
+            expire: new Date(Date.now() + 3600000), 
+            httpOnly: true, 
+            secure: false // Aggiungere { secure: true } per abilitare l'utilizzo dei cookie con connessioni HTTPS.
+        };
+        const prv_key = fs.readFileSync('./key/rsa.private');
+        const token = jwt.sign(payload, prv_key, option);
+        res.cookie('token', token, cookieOption).json(user); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
 });
 
 // Route per il logout [METHOD = DELETE]
